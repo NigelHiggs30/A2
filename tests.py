3450650c98cdebcc8a06728d2c8ae1e426ced6bc
NigelHiggs30 <higgsn@oregonstate.edu>
2023-02-20 20:13:22 -0800

    test2 check to see if a string is longer than 0 but should be a minimum of len 8

--- tests.py
+++ tests.py
@@ -6,5 +6,8 @@ class TestCase(unittest.TestCase):
     def test1(self):
         input = ""
         self.assertFalse(check_pwd(input))
+    def test2(self):
+        input = "0"
+        self.assertFalse(check_pwd(input))
 if __name__ == "__main__":
     unittest.main()
