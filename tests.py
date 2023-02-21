5ba1ce20dc2a2360b2e4d531e62388a7edda2a63
NigelHiggs30 <higgsn@oregonstate.edu>
2023-02-20 20:08:15 -0800

    test1 check if empty string handled correctly

--- tests.py
+++ tests.py
@@ -3,6 +3,8 @@ from check_pwd import check_pwd
 
 
 class TestCase(unittest.TestCase):
-    pass
+    def test1(self):
+        input = ""
+        self.assertFalse(check_pwd(input))
 if __name__ == "__main__":
     unittest.main()
